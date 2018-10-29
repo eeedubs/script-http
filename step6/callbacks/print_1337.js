673a68fe9251a03aec4e05760d8932c1c326f90f
Eric Wilson <ericswilson94@gmail.com>
2018-10-29 21:49:41 +0000

    updated step 6

--- step6/callbacks/print_1337.js
+++ step6/callbacks/print_1337.js
@@ -5,8 +5,32 @@ var requestOptions = {
   path: '/http-examples/step5.html'
 };
 
-function printUpperCase (html) {
-  console.log(html.toUpperCase())
+function printLeet (html) {
+  var newArray = [];
+  for (var x = 0; x < html.length; x++){
+    if (html.charAt(x) === "a"){
+      newArray.push("4");
+    }
+    else if (html.charAt(x) === "e"){
+      newArray.push("3");
+    }
+    else if (html.charAt(x) === "o"){
+      newArray.push("0");
+    }
+    else if (html.charAt(x) === "l"){
+      newArray.push("1");
+    }
+    else if (html.charAt(x) === "s"){
+      newArray.push("5");
+    }
+    else if (html.charAt(x) === "t"){
+      newArray.push("7");
+    }
+    else {
+      newArray.push(html.charAt(x));
+    }
+  }
+  console.log(newArray.join(""));
 }
 
-getHTML(requestOptions, printUpperCase);
+getHTML(requestOptions, printLeet);
